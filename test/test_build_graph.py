afa1f91212a9d7450452d5fe6e15fba29ba7b752
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-21 18:13:50 +0100

    fix tests: edges may be reversed when handled by networkx

--- test/test_build_graph.py
+++ test/test_build_graph.py
@@ -7,6 +7,6 @@ from build_graph import graph_from_dirty_lines, graph_from_lines
 
 def test_equality_of_methods(data_bad_complex):
     lines = data_bad_complex.splitlines()
-    one = graph_from_dirty_lines(lines)
-    two = graph_from_lines(lines)
-    assert set(one.edges) == set(two.edges)
+    one = map(frozenset, graph_from_dirty_lines(lines).edges)
+    two = map(frozenset, graph_from_lines(lines).edges)
+    assert set(one) == set(two)
