034dffecdfb823413f806641c4a5322afe58564c
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-21 18:51:06 +0100

    implement info command

--- routines.py
+++ routines.py
@@ -30,5 +30,33 @@ def split_by_cc(fname:str, targets:str=None, edge_predicate:str=edge_predicate) 
     return tuple(writtens)
 
 
+def info(fname:str, info_motifs:int=0, info_ccs:bool=True,
+         edge_predicate:str=edge_predicate) -> dict:
+    """Yield (field, value) infos of targets written
 
-split_by_cc('data/three_cc.lp', 'out/three_cc_{}.lp')
+    info_motifs -- print info about the n first motifs in the graph
+    info_ccs -- print info about connected components in the graph
+
+    """
+    graph = graph_from_file(fname, edge_predicate=edge_predicate)
+    nb_node, nb_edge = len(graph.nodes), len(graph.edges)
+    def density(nb_node, nb_edge):
+        return 2 * nb_edge / (nb_node * (nb_node - 1))
+
+    yield '#node', nb_node
+    yield '#edge', nb_edge
+    yield 'density', density(nb_node, nb_edge)
+
+    if info_motifs:
+        for motif in ():
+            clyngor.solve()
+    if info_ccs:
+        ccs_nodes = tuple(networkx.connected_components(graph))
+        ccs = tuple(graph.subgraph(cc) for cc in ccs_nodes)
+        yield '#cc', len(ccs_nodes)
+        node_per_cc = tuple(map(len, ccs_nodes))
+        yield '#node/cc', node_per_cc
+        yield '#node/cc (prop)', tuple(nb / nb_node for nb in node_per_cc)
+        yield '#node/cc (mean)', sum(node_per_cc) / len(node_per_cc)
+        yield 'density/cc', tuple(density(len(nodes), len(tuple(cc.edges))) for cc, nodes in zip(ccs, ccs_nodes))
+
