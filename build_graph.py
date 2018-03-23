5855b5c35f41b62c45430d88d6d0b19a7d70d3e7
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-23 15:53:40 +0100

    implement graph to file function

--- build_graph.py
+++ build_graph.py
@@ -1,5 +1,6 @@
 
 import networkx
+from asp import asp_from_graph
 from commons import edge_predicate
 from extract_links import links_from_file, links_from_dirty_file
 from extract_links import links_from_lines, links_from_dirty_lines
@@ -29,3 +30,10 @@ def graph_from_dirty_lines(lines:iter, edge_predicate:str=edge_predicate):
     for edge in links_from_dirty_lines(lines, edge_predicate=edge_predicate):
         graph.add_edge(*edge)
     return graph
+
+
+def graph_to_file(graph, fname:str, edge_predicate:str=edge_predicate, eol:str='\n'):
+    """Write given graph into file, in clean ASP format."""
+    with open(fname, 'w') as fd:
+        for line in asp_from_graph(graph, edge_predicate=edge_predicate):
+            fd.write(line + eol)
