10657cfa2d969f15156eb229f9b5b42720a3afb2
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-03-23 15:58:21 +0100

    provides a path normalization method

--- commons.py
+++ commons.py
@@ -1,3 +1,18 @@
 
+import os
 
 edge_predicate = 'edge'
+
+
+def normalize_filename(fname:str) -> str:
+    """Return filename that hopefully is non ambiguous.
+    """
+    funcs = (
+        os.path.expanduser,
+        os.path.expandvars,
+        os.path.normpath,
+        os.path.abspath,
+    )
+    for func in funcs:
+        fname = func(fname)
+    return fname
