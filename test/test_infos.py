f0707e259659878373a6cc2e875a3169c5bcc9c5
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-05-04 17:57:32 +0200

    fix 2 of 3 failing tests

--- test/test_infos.py
+++ test/test_infos.py
@@ -12,34 +12,34 @@ def test_infos_with_properties_and_negative():
 
 
 EXPECTED_SIMPLE = """
-   #node | 11
-   #edge | 19
- density | 0.34545454545454546
-     #cc | 1
+    #node | 11
+    #edge | 19
+  density | 0.34545454545454546
+      #cc | 1
 """.splitlines(False)[1:]
 
 EXPECTED_PROPERTIES = """
-                        #node | 11
-                        #edge | 19
-                      density | 0.35
-                          #cc | 1
-                 transitivity | 0.59
-           average_clustering | 0.6
-    average_node_connectivity | 1.78
- average_shortest_path_length | 2.0
-                   properties | chordal, connected
+                         #node | 11
+                         #edge | 19
+                       density | 0.35
+                           #cc | 1
+                  transitivity | 0.59
+            average_clustering | 0.6
+     average_node_connectivity | 1.78
+  average_shortest_path_length | 2.0
+                    properties | chordal, connected, no loop
 """.splitlines(False)[1:]
 
 EXPECTED_PROPERTIES_AND_NEGATIVES = """
-                        #node | 11
-                        #edge | 19
-                      density | 0.35
-                          #cc | 1
-                 transitivity | 0.59
-           average_clustering | 0.6
-    average_node_connectivity | 1.78
- average_shortest_path_length | 2.0
-              non implemented | aperiodic, arborescence, attracting_component, branching, semiconnected, strongly_connected, weakly_connected
-                  ¬properties | biconnected, bipartite, directed, directed_acyclic_graph, distance_regular, empty, eulerian, forest, frozen, strongly_regular, tree
-                   properties | chordal, connected
+                         #node | 11
+                         #edge | 19
+                       density | 0.35
+                           #cc | 1
+                  transitivity | 0.59
+            average_clustering | 0.6
+     average_node_connectivity | 1.78
+  average_shortest_path_length | 2.0
+               non implemented | aperiodic, arborescence, attracting_component, branching, semiconnected, strongly_connected, weakly_connected
+                   ¬properties | biconnected, bipartite, directed, directed_acyclic_graph, distance_regular, empty, eulerian, forest, frozen, strongly_regular, tree
+                    properties | chordal, connected, no loop
 """.splitlines(False)[1:]
